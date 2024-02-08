9ffb064b76674784f7984a0347a20f6ad7f2cf0f
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2024-02-08 15:05:59 +0100

    Factorisation (création des fonctions) pour le changement de position

--- js/cv.app.js
+++ js/cv.app.js
@@ -1,3 +1,28 @@
+function secondPositionToFirstPosition (nodeSelected, nodeToPlaceInSecondPosition, nodeToPlaceInthirdPosition) {
+    nodeSelected.classList.add('firstPosition');
+    nodeSelected.classList.remove('secondPosition', 'thirdPosition');
+    placeNodeInSecondPosition(nodeToPlaceInSecondPosition);
+    placeNodeInThirdPosition(nodeToPlaceInthirdPosition);
+}
+
+function thirdPositionToFirstPosition (nodeSelected, nodeToPlaceInSecondPosition, nodeToPlaceInthirdPosition) {
+    nodeSelected.classList.add('firstPosition');
+    nodeSelected.classList.remove('secondPosition', 'thirdPosition');
+    placeNodeInSecondPosition(nodeToPlaceInSecondPosition);
+    placeNodeInThirdPosition(nodeToPlaceInthirdPosition);
+}
+
+function placeNodeInSecondPosition (nodeSelected) {
+    nodeSelected.classList.add('secondPosition');
+    nodeSelected.classList.remove('firstPosition', 'thirdPosition');
+}
+
+function placeNodeInThirdPosition (nodeSelected) {
+    nodeSelected.classList.add('thirdPosition');
+    nodeSelected.classList.remove('firstPosition', 'secondPosition');
+}
+
+
 let corps = document.querySelector('body');
 
 corps.addEventListener('click', function(e) {
@@ -8,49 +33,48 @@ corps.addEventListener('click', function(e) {
         classesRecuperees = e.target.parentNode.className.split(' ');
     }
 
+    let classeConcernee = classesRecuperees.includes('presentation') ? '.presentation' : classesRecuperees.includes('projets') ? '.projets' : classesRecuperees.includes('competences') ? '.competences': false;
 
-    let classeConcernee = classesRecuperees.includes('presentation') ? '.presentation' : classesRecuperees.includes('projets') ? '.projets' : '.competences';
     let divPresentation = document.querySelector('.presentation'), divProjets = document.querySelector('.projets'), divCompetences = document.querySelector('.competences');
 
+    
     switch (classeConcernee) {
+
         case '.presentation':
-            if (divPresentation.classList.contains('firstPosition')) {
-                // divPresentation.classList.add('firstPosition');
-                // divPresentation.classList.remove('secondPosition', 'thirdPosition');
+            if (!divPresentation.classList.contains('firstPosition') && divPresentation.classList.contains('secondPosition')) {
+                secondPositionToFirstPosition(divPresentation, divProjets, divCompetences);
+            } else if (!divPresentation.classList.contains('firstPosition') && divPresentation.classList.contains('thirdPosition')) {
+                thirdPositionToFirstPosition(divPresentation, divProjets, divCompetences);
             }
-
-            divProjets.classList.add('secondPosition');
-            divProjets.classList.remove('firstPosition', 'thirdPosition');
-
-            divCompetences.classList.add('thirdPosition');
-            divCompetences.classList.remove('firstPosition', 'secondPosition');
         break;
 
         case '.projets':
-            if(!divProjets.classList.contains('firstPosition')) {
-                divProjets.classList.add('firstPosition');
-                divProjets.classList.remove('secondPosition', 'thirdPosition');
+            if (!divProjets.classList.contains('firstPosition') && divProjets.classList.contains('secondPosition')) {
+                secondPositionToFirstPosition(divProjets, divCompetences, divPresentation);
+            } else if (!divProjets.classList.contains('firstPosition') && divProjets.classList.contains('thirdPosition')) {
+                thirdPositionToFirstPosition(divProjets, divCompetences, divPresentation);
             }
-
-            divCompetences.classList.add('secondPosition');
-            divCompetences.classList.remove('firstPosition', 'thirdPosition');
-
-            divPresentation.classList.add('thirdPosition');
-            divPresentation.classList.remove('firstPosition', 'secondPosition');
         break;
 
         case '.competences':
-            if(!divCompetences.classList.contains('firstPosition')) {
-                divCompetences.classList.add('firstPosition');
-                divCompetences.classList.remove('secondPosition', 'thirdPosition');
+            if (!divCompetences.classList.contains('firstPosition') && divCompetences.classList.contains('secondPosition')) {
+                secondPositionToFirstPosition(divCompetences, divPresentation, divProjets);
+            } else if (!divCompetences.classList.contains('firstPosition') && divCompetences.classList.contains('thirdPosition')) {
+                thirdPositionToFirstPosition(divCompetences, divPresentation, divProjets);
             }
-
-            divPresentation.classList.add('secondPosition');
-            divPresentation.classList.remove('firstPosition', 'thirdPosition');
-
-            divProjets.classList.add('thirdPosition');
-            divProjets.classList.remove('firstPosition', 'secondPosition');
         break;
     }
 
+})
+
+let box = document.querySelector('#boxtext');
+
+box.addEventListener('click', function() {
+    box.style.animationPlayState = 'running'; 
+    setTimeout(function() {
+        box.style.top = '100px';
+        box.style.left = '100px';
+    }, 2000);
+    
+    
 })
