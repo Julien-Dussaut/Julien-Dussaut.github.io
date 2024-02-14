ffd87ddb204a897bd9b3e0bbc237da486be2630f
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2024-02-14 14:18:22 +0100

    Ajout du défilement doux lors du clic sur les ancres.

---
+++ js/main.script.js
@@ -0,0 +1,9 @@
+let anchorLink = document.querySelectorAll('.internLink');
+
+anchorLink.forEach(currentNode => {
+
+    currentNode.addEventListener('click', function() {
+        document.querySelector(currentNode.href.slice(currentNode.href.indexOf('#'))).scrollIntoView();
+    })
+    
+});
