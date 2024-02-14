2035b873ae78f29d40c638e8b6c898f7e141fd51
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2024-02-14 14:23:58 +0100

    Ajout de la gestion des smartphones en mode paysage et ajout du défilement doux pour les smartphones.

--- js/main.script.js
+++ js/main.script.js
@@ -3,7 +3,7 @@ let anchorLink = document.querySelectorAll('.internLink');
 anchorLink.forEach(currentNode => {
 
     currentNode.addEventListener('click', function() {
-        document.querySelector(currentNode.href.slice(currentNode.href.indexOf('#'))).scrollIntoView();
+        document.querySelector(currentNode.href.slice(currentNode.href.indexOf('#'))).scrollIntoView({behavior: "smooth"});
     })
     
 });
