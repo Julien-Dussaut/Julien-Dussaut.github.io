90ba4477677920b0e5414738d6fed2f948d74d7c
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2024-02-14 15:30:16 +0100

    Modification des ancres et du comportement du header lors du scroll.

--- js/main.script.js
+++ js/main.script.js
@@ -1,3 +1,13 @@
+/* window.addEventListener('scroll', function(scroll) {
+
+    if(window.scrollY / this.document.body.clientHeight > 0) {
+        document.querySelector('header').style.backgroundColor = '#011222';
+    } else {
+        document.querySelector('header').style.backgroundColor = '';
+    }
+    
+}) */
+
 let anchorLink = document.querySelectorAll('.internLink');
 
 anchorLink.forEach(currentNode => {
